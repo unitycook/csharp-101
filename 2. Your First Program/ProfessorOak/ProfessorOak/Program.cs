fc26e93efaa9c453e2c7b7e03a9f48496e986788
sainthkh <sainthkh@naver.com>
2018-11-16 17:38:38 +0900

    Added chapter 2 examples.

---
+++ 2. Your First Program/ProfessorOak/ProfessorOak/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProfessorOak
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World");
+            Console.WriteLine("I’m Professor Oak.");
+            Console.WriteLine("What is your name ?");
+
+            var yourName = Console.ReadLine();
+
+            Console.WriteLine("Oh, your name is " + yourName);
+
+            if (yourName == "Ash")
+            {
+                Console.WriteLine("Here is your Pikachu.");
+            }
+            else
+            {
+                Console.WriteLine("Here is your Charmander");
+            }
+        }
+    }
+}
